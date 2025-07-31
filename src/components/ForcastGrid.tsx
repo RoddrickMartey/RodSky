5969957aaec9e059a5cd27173d4a010b46316070
Roddrick Martey Anum <162825273+RoddrickMartey@users.noreply.github.com>
2025-07-31 12:34:25 +0000

    Update ForcastGrid.tsx

--- src/components/ForcastGrid.tsx
+++ src/components/ForcastGrid.tsx
@@ -1,4 +1,3 @@
-// ForecastGrid.tsx
 import ForecastItem from "./ForcastItem";
 
 function ForcastGrid({
@@ -15,14 +14,16 @@ function ForcastGrid({
   const heading = `This week's Forecast (${forcast[0].date})`;
 
   return (
-    <section className="space-y-4 rounded-lg bg-card p-6 shadow-sm border-border border">
-      <div className=" pb-2">
+    <section className="flex flex-col h-full rounded-lg bg-card p-6 shadow-sm border border-border space-y-4">
+      {/* Heading */}
+      <div>
         <h2 className="text-lg font-semibold text-muted-foreground">
           {heading}
         </h2>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4">
+      {/* Forecast Items */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4 flex-1">
         {forcast.map((item, idx) => (
           <ForecastItem item={item} key={idx} />
         ))}
